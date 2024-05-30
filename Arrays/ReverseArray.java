5b0804cd9b6cc5fcec519b3801054d43904de6d9
ckss2k21 <chandan.kumar012k@gmail.com>
2024-05-30 18:53:25 +0530

    FIrst commit

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,65 @@
+package Arrays;
+
+/* Problem : Given Multiple testCase t, Every testCases has array of size n. 
+*  Task is to  Reverse the array
+*  Input : 
+   2
+   5 
+   1 2 3 4 5
+   10 
+   1 2 3 4 5 6 7 8 9 10
+   
+   Output : 
+   5 4 3 2 1
+   10 9 8 7 6 5 4 3 2 1
+*/
+
+import java.io.*;
+import java.util.*;
+class ReverseArray{
+
+	public void swap(int arr[], int i , int j)
+	{
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+    public void reverseArray(int arr[], int n)
+    {
+        for(int i = 0 , j = n-1; i < j; i++, j--)
+        {
+            swap(arr, i, j);
+        }
+    }
+    void printArray(int arr[])
+    {
+        for(int i=0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
+
+class Launch {
+    public static void main(String[] args)
+    {
+        Scanner sc  = new Scanner(System.in);
+        int t = sc.nextInt();
+        ReverseArray rev = new ReverseArray();
+        while( t > 0)
+        {
+            int n = sc.nextInt();
+            int arr[] = new int [n];
+            for(int i = 0; i < n; i++)
+            {
+            	arr[i] = sc.nextInt();
+            }
+            rev.reverseArray(arr,n);
+            rev.printArray(arr);
+            t--;
+        }
+        
+    }
+}
